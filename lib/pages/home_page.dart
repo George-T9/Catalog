ccb5211c1449be0112a126a942ce48691d66aaba
George Toppo <gtoppo99@outlook.in>
2021-11-28 21:23:31 +0530

    Updating UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -21,27 +21,34 @@ class _HomePageState extends State<HomePage> {
   loadData() async {
     Future.delayed(Duration(seconds: 2));
     final catalogJson =
-    await rootBundle.loadString("assets/files/catalog.json");
+        await rootBundle.loadString("assets/files/catalog.json");
     final decodedData = jsonDecode(catalogJson);
     final productData = decodedData["products"];
     CatalogModel.items =
         List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {
+
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            CatalogHeader(),
-            if(CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-              Expanded(child: CatalogList())
-            else
-              Center(
-                child: CircularProgressIndicator(),
-              )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16,8,16,2),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              CatalogHeader(),
+              if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+                Expanded(child: CatalogList())
+              else
+                Center(
+                  child: CircularProgressIndicator(),
+                )
+            ],
+          ),
         ),
       ),
     );
@@ -53,14 +60,18 @@ class CatalogHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text(
-          'Catalog App',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-        ),
-        Text('Trending products', style: TextStyle(fontSize: 12)),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Catalog App',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+          ),
+          Text('Trending products', style: TextStyle(fontSize: 16)),
+        ],
+      ),
     );
   }
 }
@@ -96,9 +107,40 @@ class CatalogItem extends StatelessWidget {
     return Card(
       child: Row(
         children: <Widget>[
-
+          CatalogImage(image: catalog.image),
+          Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 16,),
+              Text(catalog.name,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16.0),),
+              Text(catalog.desc,style: TextStyle(fontSize: 16,)),
+              ButtonBar(
+                alignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("\$${catalog.price}",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16),),
+                  ElevatedButton(onPressed: () {}, child: Text('Buy'))
+                ],
+              )
+            ],
+          ))
         ],
       ),
+    );
+  }
+}
+
+class CatalogImage extends StatelessWidget {
+  final String image;
+
+  const CatalogImage({Key? key, required this.image}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(12.0,2.0,12.0,2.0),
+      child: Image.network(image,fit: BoxFit.cover,width: 60,),
     );
   }
 }
